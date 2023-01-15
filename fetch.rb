f1b9893ae1ab9962a5b5df86289f54108aed0aaf
thekuwayama <thekuwayama@gmail.com>
2023-01-15 21:32:09 +0900

    feat: print format

--- fetch.rb
+++ fetch.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# typed: false
 # frozen_string_literal: true
 
 require 'openssl'
@@ -55,11 +56,36 @@ if ARGV.empty?
 end
 
 resolve(ARGV[0], SERVER, PORT, HOSTNAME).each do |rr|
-  next unless rr.kind_of?(Resolv::DNS::Resource::IN::HTTPS)
+  next unless rr.is_a?(Resolv::DNS::Resource::IN::HTTPS)
   next unless rr.svc_params.keys.include?('ech')
 
-  pp rr.svc_params['ech']
-end
+  echconfiglist = rr.svc_params['ech'].echconfiglist
+  puts '---------- hex dump ----------'
+  echconfiglist.each_with_index do |c, i|
+    puts "ECHConfig #{i}"
+    puts c.encode.unpack1('H*').scan(/.{2}/).each_slice(8).map { |a| a.join(' ') }.each_slice(2).map { |a| a.join('      ') + "\n" }.join
+  end
 
-# TODO: refine ech_config to pp
-# https://www.ietf.org/archive/id/draft-ietf-tls-esni-15.html#section-4-2
+  puts '----------- fields -----------'
+  # https://www.ietf.org/archive/id/draft-ietf-tls-esni-15.html#section-4-2
+  echconfiglist.each do |c|
+    puts 'ECHConfig:'
+    puts "  version(uint16):\t\t\t#{c.version.unpack1('H4').scan(/.{2}/).join(' ')}"
+    echconfig_contents = c.echconfig_contents
+    puts "  length(uint16):\t\t\t#{echconfig_contents.encode.length}"
+    puts '  contents(ECHConfigContents):'
+    puts '    key_config(HpkeKeyConfig):'
+    key_config = echconfig_contents.key_config
+    puts "      config_id(uint8):\t\t\t#{key_config.config_id}"
+    puts "      kem_id(uint16):\t\t\t#{key_config.kem_id.encode.unpack1('H4').scan(/.{2}/).join(' ')}"
+    puts "      public_key(opaque):\t\t#{key_config.public_key.opaque.unpack1('H*').scan(/.{2}/).join(' ')}"
+    puts '      cipher_suites(HpkeSymmetricCipherSuite):'
+    key_config.cipher_suites.each do |cs|
+      puts "        kdf_id(uint16):\t\t\t#{cs.kdf_id.encode.unpack1('H4').scan(/.{2}/).join(' ')}"
+      puts "        aead_id(uint16):\t\t#{cs.aead_id.encode.unpack1('H4').scan(/.{2}/).join(' ')}"
+    end
+    puts "    maximum_name_length(uint8):\t\t#{echconfig_contents.maximum_name_length}"
+    puts "    public_name(opaque):\t\t#{echconfig_contents.public_name}"
+    puts "    extensions(opaque<0..2^16-1>):\t#{echconfig_contents.extensions.octet.then { |s| [s.length].pack('n') + s }.unpack1('H*').scan(/.{2}/).join(' ')}"
+  end
+end
